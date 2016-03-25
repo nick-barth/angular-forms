dad510449c03356004a2528e1294dfc93bc3e817
Nick Barth <barth.nicholas@gmail.com>
2016-03-25 16:06:21 -0400

    k, should be okay, still a lot to do, but almost out of time.

--- js.js
+++ js.js
@@ -1,63 +1,78 @@
-var app = angular.module('SurveyApp', []);
+//TODO: a ton of quality of life adjustments, cursor moving, tab indexing, highlighting, scrolling on new survey, things of that nature
+// a stylesheet would be nice
 
-var INTEGER_REGEXP = /^\-?\d+$/;
-app.directive('integer', function() {
-  return {
-    require: 'ngModel',
-    link: function(scope, elm, attrs, ctrl) {
-      ctrl.$validators.integer = function(modelValue, viewValue) {
-        if (ctrl.$isEmpty(modelValue)) {
-          // consider empty models to be valid
-          return true;
+(function(angular) {
+  'use strict';
+  var app = angular.module('SurveyApp', []);
+
+
+  //quickest way that came to mind was just to regex it as a string to make sure it's a positive integer, maybe a bit hacky
+  //will have to be converted to int on backend? maybe? todo: do it on the frontend
+  var INTEGER_REGEXP = /^\+?(0|[1-9]\d*)$/;
+  app.directive("integer", function() {
+    return {
+      restrict: "A",
+      require: "ngModel",
+      link: function(scope, element, attributes, ngModel) {
+        ngModel.$validators.integer = function(modelValue) {
+          if (INTEGER_REGEXP.test(modelValue)) {
+            return true;
+          } else {
+            return false;
+          }
         }
-
-        if (INTEGER_REGEXP.test(viewValue)) {
-          // it is valid
-          return true;
-        }
-
-        // it is invalid
-        return false;
-      };
-    }
-  };
-});
+      }
+    };
+  });
 
 
-app.controller('FormCtrl', function($scope) {
+  app.controller('FormCtrl', function($scope) {
+    $scope.questions = [{
+      qID: 1,
+      title: 'Example question 1'
+    }];
+    $scope.surveys = [{
+      title: 'Survey 1',
+      pointValue: 25,
+      desc: 'A really great survey that does really great things',
+      questions: $scope.questions,
+      id: 1
+    }];
 
-  $scope.questions = [{
-    num: 1,
-    q: 'Example question 1'
-  }];
-  $scope.surveys = [{
-    name: 'Survey 1',
-    points: 25,
-    desc: 'A really great survey that does really great things',
-    qs: $scope.questions,
-    id: 1
-  }];
+    //todo: remove question
+    $scope.addNewQuestion = function(id) {
+      var newQuestionNum = $scope.surveys[id - 1].questions.length + 1;
+      $scope.surveys[id - 1].questions.push({
+        'qID': newQuestionNum,
+        title: 'Example Question ' + newQuestionNum
+      });
+    };
 
-  $scope.addNewQuestion = function(id) {
-    var newQuestionNum = $scope.surveys[id - 1].qs.length + 1;
-    $scope.surveys[id - 1].qs.push({
-      'num': newQuestionNum,
-      q: 'Example Question ' + newQuestionNum
-    });
-  };
+    //todo: remove survey
+    $scope.addNewSurvey = function() {
+      var newSurveyID = $scope.surveys.length + 1;
+      $scope.surveys.push({
+        id: newSurveyID,
+        title: 'Survey ' + newSurveyID,
+        pointValue: 50,
+        desc: 'Alright, another survey, you like that?',
+        questions: [{
+          qID: 1,
+          title: 'Example Question 1',
+        }],
+      });
+    };
 
-  $scope.addNewSurvey = function() {
-    var newSurveyID = $scope.surveys.length + 1;
-    $scope.surveys.push({
-      id: newSurveyID,
-      name: 'Survey ' + newSurveyID,
-      points: 50,
-      desc: 'Alright, another survey, you like that?',
-      qs: [{
-        num: 1,
-        q: 'Example Question 1',
-      }],
-    });
-  };
+    $scope.submit = function(form) {
+      if (form.$valid) {
+        //todo:actually have a post function
+        var post = angular.toJson($scope.surveys);
+        console.log(post);
+      } else {
+        //todo: better highlight of errors, running short on time.
+        alert('fix your errors bud');
+      }
+    };
+  });
 
-});
+})(window.angular);
